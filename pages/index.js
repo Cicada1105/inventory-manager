362dd396abc1c6e527c0b87740d470edfa9ca8e4
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-08-28 19:06:29 -0400

    Initialized NextJS files and included TailwindCSS using basic example from Tailwind documentation

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className="text-center mt-3 text-3xl font-bold underline">
+      Inventory Manager
+    </h1>
+  )
+}
