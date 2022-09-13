d32a266c98f9d50f0de26162b392da419c76cd69
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-09-13 11:36:22 -0400

    Connected local environment to MongoDB Atlas cluster

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import mongoClient from '../utils/mongodb.js'
 
-export default function Home() {
+export default function Home({ types }) {
+
   return (
-    <h1 className="text-center mt-3 text-3xl font-bold underline">
-      Inventory Manager
-    </h1>
+    <>
+      <h1 className="text-center mt-3 text-3xl font-bold underline">
+        Inventory Manager
+      </h1>
+      <dl style={{color:"white"}}>
+      {
+        JSON.parse(types).map((type,i) => {
+          return (
+            <div key={i}>
+              <dt>{type.name}</dt>
+              <dd>{type.restriction}</dd>
+            </div>
+          );
+        })
+      }
+      </dl>
+    </>
   )
+}
+
+export async function getServerSideProps(context) {
+  let client;
+  try {
+    // Await the connection to the MongoDB URI
+    client = await mongoClient.connect();
+
+    let db = client.db(process.env.MONGODB_DB);
+    //let collection = await db.collection("access_types");
+    //console.log(collection);
+    let types = await db.collection("access_types").find({}).toArray();
+    console.log(types);
+
+    return {
+      props: {
+        types: JSON.stringify(types)
+        //types: JSON.stringify([{name:"Basic User", restriction:1},{name:"Admin", restriction: 2},{name:"Super User", restriction:3}])
+      }
+    }
+  } catch(e) {
+    console.error(e);
+    return {
+      props: {
+        types: []
+      }
+    }
+  } finally {
+    // End connection after closing of app or error
+    await client.close();
+  }
 }
---
+++ utils/mongodb.js
@@ -0,0 +1,18 @@
+/*
+	Used https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
+	as reference
+*/
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+// Check if the mongodb uri exists in the local env
+if (!process.env.MONGODB_URI) {
+	throw new Error("Missing MONGODB_URI environment variable");
+}
+
+const mongoURI = process.env.MONGODB_URI;
+const options = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 };
+
+let client = new MongoClient(mongoURI, options);
+
+export default client;
